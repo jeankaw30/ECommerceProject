3a2eea8e0f3dccb667c5ed3a401c4f84ef31bd10
JEANNA KAWAMOTO <kawzie30@live.com>
2018-12-04 00:31:39 -0800

    SET UP / START OF PROJECT

---
+++ ECommerce.Entities/Product.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ECommerce.Entities
+{
+    public class Product : BaseEntity
+    {
+        public decimal Price { get; set; }
+        public Category Category { get; set; }
+        //public int ID { get; set; }
+        //public string Name { get; set; }
+        //public string Description { get; set; }
+        
+    }
+}
---
+++ ECommerceDatabase/DBContext.cs
@@ -0,0 +1,19 @@
+﻿using ECommerce.Entities;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks; 
+
+namespace ECommerceDatabase
+{
+    public class  DBContext : DbContext
+    {
+        public DBContext() : base("ECommerceConnection")
+        {
+        } 
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<Product> Products { get; set; }
+    }
+}
