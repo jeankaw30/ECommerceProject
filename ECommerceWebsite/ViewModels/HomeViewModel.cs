e2c9e9c8df360248b48c32832a67a13cb940eb73
JEANNA KAWAMOTO <kawzie30@live.com>
2018-12-09 22:19:33 -0800

    Added Featured Product Image to Featured Categoies Does not work until images are  uploaded when creating category form used. Need to finish adding upload photos functionality

---
+++ ECommerceWebsite/ViewModels/HomeViewModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using ECommerce.Entities;
+
+namespace ECommerceWebsite.ViewModels
+{
+    public class HomeViewModel
+    {
+        public List<Category> FeaturedCategories { get; set; }
+        public List<Product> FeaturedProducts { get; set; }
+
+    }
+}
